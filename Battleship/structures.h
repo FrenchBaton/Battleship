cd2235ff97b495ebb5cc3bb6f3d34dbd45eede4f
hdgehog <hedgehogagaga@gmail.com>
2026-05-11 12:57:02 +0300

    add grid cell and position structures

--- Battleship/structures.h
+++ Battleship/structures.h
@@ -1,2 +1,14 @@
 #pragma once
 // структуры
+typedef unsigned int uint;
+
+struct GridСell {
+	Position pos_cell;
+	char show_symb = '~';
+	char gen_symb = '~';
+};
+
+struct Position {
+	uint x;
+	uint y;
+};
